2c36973cc51e5a1fb1bce12d502a99eab615650b
Hunter Pruitt <hwpruitt@ncsu.edu>
2022-06-04 02:03:37 -0400

    [Test] Created AnimeTest

---
+++ AnimeLog/test/anime/AnimeTest.java
@@ -0,0 +1,100 @@
+package anime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests the Anime class
+ * @author Hunter Pruitt
+ */
+class AnimeTest {
+
+	
+	//TODO: Test finished and dropped at same time 
+	@Test
+	void testAnime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLanguage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetLanguage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsFinished() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFinished() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsDropped() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDropped() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDirector() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDirector() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNotes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetNotes() {
+		fail("Not yet implemented");
+	}
+
+}
