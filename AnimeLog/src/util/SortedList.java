114970cd40e33615b47b73f202f8eefb9faff8e8
Hunter Pruitt <hwpruitt@ncsu.edu>
2022-06-07 21:49:27 -0400

    [Implementation][Test] Finished SortedList and SortedListTest

--- AnimeLog/src/util/SortedList.java
+++ AnimeLog/src/util/SortedList.java
@@ -1,9 +1,9 @@
 package util;
 
 /**
- * List for storing objects alphabetically,
+ * List for storing objects in a sorted order,
  * modeled after a LinkedList.
- * Allows duplicates. 
+ * Does not allow duplicates. 
  * @author Hunter Pruitt
  *
  * @param <E> type of data to be stored in the list
@@ -24,32 +24,142 @@ public class SortedList<E extends Comparable<E>> {
 		front = null;
 	}
 	
-	public boolean add(E element) {
-		//If list is empty
-		front = new ListNode(element);
-		
-		// TODO Auto-generated method stub
-		return false;
-	}
+	/**
+	 * Adds the element to the list in sorted order.
+	 * @param element element to add
+	 * @throws NullPointerException if element is null
+	 * @throws IllegalArgumentException if element is duplicate 
+	 */
+	public void add(E element) {
 
-	public E remove(int idx) {
-		// TODO Auto-generated method stub
-		return null;
+		//Check can add
+		if (element == null) {
+			throw new NullPointerException("Cannot add null element.");
+		} else if (contains(element)) {
+			throw new IllegalArgumentException("Cannot add duplicate element.");
+		}
+		
+		
+		//Add as front if list is empty
+		if (front == null) {
+			front = new ListNode(element);
+			size++;
+		} else {
+			
+			//If need to add before front
+			if (0 < front.data.compareTo(element)) {
+				front = new ListNode(element, front);
+				size++;
+				return;
+			}
+			
+			//Seek through nodes, seeing if need to insert before an element
+			ListNode temp = front;
+			
+			while(temp.next != null) {
+
+				if (0 < temp.next.data.compareTo(element)) {
+					//Add element before the next element
+					temp.next = new ListNode(element, temp.next);
+					size++;
+					return;
+				}
+				temp = temp.next;
+			}
+
+			//Add at end if not found yet
+			temp.next = new ListNode(element);
+			size++;
+		} //else clause	
 	}
 	
+	/**
+	 * Returns the element from the given index.  The element is
+	 * removed from the list.
+	 * @param idx index to remove element from
+	 * @return element at given index
+	 * @throws IndexOutOfBoundsException if the idx is out of bounds for the list
+	 */
+	public E remove(int idx) {
+		if(idx < 0 || idx >= size) {
+			throw new IndexOutOfBoundsException("Invalid index.");
+		}
+		
+		E removed;
+	
+		//Remove from front
+		if(idx == 0) {
+			removed = front.data;
+			front = front.next;
+		}
+		//Remove from middle
+		else if (idx != size - 1) {
+			ListNode temp = front;
+			for (int i = 1; i < idx; i++) {
+				temp = temp.next;
+			}
+			removed = temp.next.data;
+			temp.next = temp.next.next;
+		}
+		//Remove from end
+		else {
+			ListNode temp = front;
+			for (int i = 1; i < idx; i++) {
+				temp = temp.next;
+			}
+			removed = temp.next.data;
+			temp.next = null;
+		}
+		
+		size--;
+		return removed;
+	}	
+
+	
+	/**
+	 * Returns true if the element is in the list.
+	 * @param element element to search for
+	 * @return true if element is found
+	 */
 	public boolean contains(E element) {
+		ListNode temp = front;
+		//Return false if the list is empty
+		if (front == null) {
+			return false;
+		}
+		
+		//Check if the element is the first element
+		if (front.data.equals(element)) {
+			return true;
+		}
+		//Else check to see if the list is contained in any node
+		while(temp.next != null) {
+			if (temp.data.equals(element)) {
+				return true;
+			} else {
+				temp = temp.next;
+			}
+		}
 		return false;
 	}
 	
 	/**
-	 * Returns an object at a specific index of the list
-	 * @return element stored at given index
+	 * Returns the element at the given index.
+	 * @param idx index of the element to retrieve
+	 * @return element at the given index
 	 * @throws IndexOutOfBoundsException if the idx is out of bounds
+	 * 		for the list
 	 */
-	public E get(int index) {
-		//
-		// TODO Auto-generated method stub
-		return null;
+	public E get(int idx) {
+		if (idx < 0 || idx >= size) {
+			throw new IndexOutOfBoundsException("Invalid index.");
+		}
+		ListNode temp = front;
+		for (int i = 0; i < idx; i++) {
+			temp = temp.next;
+		}
+
+		return temp.data;
 	}
 
 	/**
