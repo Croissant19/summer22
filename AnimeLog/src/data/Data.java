4ec0cd3d16efd36c99e9599c8fb492e1c013cca3
Hunter Pruitt <hunter.mugiwara.p@gmail.com>
2022-08-14 15:54:32 -0400

    [Update]Added code for managing Manga data to Data

--- AnimeLog/src/data/Data.java
+++ AnimeLog/src/data/Data.java
@@ -15,33 +15,50 @@ public class Data {
 	/** Reference Anime list sorted by Year */
 	private SortedMediaList animeByYear = new SortedMediaList(SortFocus.NUMERICAL);
 	
-	private Preferences preferences;
+	/** Reference Manga list sorted by Title */
+	private SortedMediaList mangaByTitle = new SortedMediaList(SortFocus.ALPHABETICAL);
+
+	/** Reference Manga list sorted by Year */
+	private SortedMediaList mangaByYear = new SortedMediaList(SortFocus.NUMERICAL);
 	
+	/** Preferences for sorting Anime */
+	private Preferences animePreferences;
+
+	/** Preferences for sorting Manga */
+	private Preferences mangaPreferences;
 	
 	/**
 	 * Constructs the Data object, setting defaults and initializing fields when they are declared
 	 */
 	public Data() {
 		//Set default preferences
-		preferences = new Preferences();
+		animePreferences = new Preferences();
+		mangaPreferences = new Preferences();
 	}
 
 	/**
 	 * Constructor compatible with imported data, sets fields to values specified in DataIO
-	 * @param animeList SortedAnimeList to overwrite default empty list fields
-	 * @param p user preferences from import
+	 * @param animeList SortedMediaList to overwrite default empty list fields
+	 * @param animeP user preferences from import
+	 * @param mangaList SortedMediaList to overwrite default empty list fields
+	 * @param mangaP user preferences from import
 	 */
-	public Data(SortedMediaList animeList, Preferences p) {
+	public Data(SortedMediaList animeList, Preferences animeP, SortedMediaList mangaList, Preferences mangaP) {
 		//Always import and export with alphabetical list
 		animeByTitle = animeList;
+		mangaByTitle = mangaList;
 		//Construct numerical list from alphabetical list
 		for (Media m : animeList) {
 			animeByYear.add(m);
 		}
+		for (Media m : mangaList) {
+			mangaByYear.add(m);
+		}
 
-		this.preferences = p;
+		animePreferences = animeP;
+		mangaPreferences = mangaP;
 	}
-	
+
 	/**
 	 * Adds an anime to both sorted lists in the data
 	 * @param a Anime to be added
@@ -54,6 +71,17 @@ public class Data {
 	}
 
 	/**
+	 * Adds a manga to both sorted lists in the data
+	 * @param m Manga to be added
+	 * @throws NullPointerException if m is null
+	 * @throws IllegalArgumentException if m is a copy of any other element
+	 */
+	public void addManga(Manga m) {
+		mangaByTitle.add(m);
+		mangaByYear.add(m);
+	}
+	
+	/**
 	 * Removes the passed Anime from both lists in data
 	 * @param a Anime to be removed
 	 * @return removed Anime
@@ -64,6 +92,17 @@ public class Data {
 		return (Anime) animeByYear.remove(animeByYear.indexOf(a));
 	}
 	
+	/**
+	 * Removes the passed Manga from both lists in data
+	 * @param m Manga to be removed
+	 * @return removed Manga
+	 * @throws IndexOutOfBoundsException if the passed index is out of bounds
+	 */
+	public Manga removeManga(Manga m) {
+		mangaByTitle.remove(mangaByTitle.indexOf(m));
+		return (Manga) mangaByYear.remove(mangaByYear.indexOf(m));
+	}
+
 	/**
 	 * Retrieves the anime list organized by title
 	 * @return SortedMediaList sorted by title
@@ -81,12 +120,35 @@ public class Data {
 	}
 	
 	/**
-	 * Returns a Preferences object with information on how data is to be displayed
-	 * @return preference data
+	 * Retrieves the manga list organized by title
+	 * @return SortedMediaList sorted by title
 	 */
-	public Preferences getPreferences() {
-		return preferences;
+	public SortedMediaList getAlphabeticalMangaList() {
+		return mangaByTitle;
 	}
 
+	/**
+	 * Retrieves the manga list organized by year
+	 * @return SortedMediaList sorted by release year
+	 */
+	public SortedMediaList getNumericalMangaList() {
+		return mangaByYear;
+	}
+	
+	/**
+	 * Returns a Preferences object with information on how Anime data is to be displayed
+	 * @return preference data
+	 */
+	public Preferences getAnimePreferences() {
+		return animePreferences;
+	}
+
+	/**
+	 * Returns a Preferences object with information on how Manga data is to be displayed
+	 * @return preference data
+	 */
+	public Preferences getMangaPreferences() {
+		return mangaPreferences;
+	}
 
 }
